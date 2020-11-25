8ef48b231d463b281ceae314764dca14f3285f1d
hoffmanaiden <hoffman.aiden@gmail.com>
2020-11-24 19:11:32 -0500

    commit pre git

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="title">
+        <span className="blueLetter">l</span>
+        <span className="greenLetter">d</span>
+        <span className="yellowLetter">r</span>
+        <span className="orangeLetter">x</span>
+        <span className="redLetter">j</span>
+        <span className="greyLetter">r</span>
+        <span className="thistleLetter">v</span>
+        <span className="pinkLetter">p</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
